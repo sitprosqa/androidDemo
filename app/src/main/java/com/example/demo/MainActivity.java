da7ea74cee4a58ebaebca4dd564752df5899db49
sitpros <sitprosqa@gmail.com>
2019-07-18 12:55:13 +0500

    Code Update

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText name=(EditText) findViewById(R.id.name);
+        final EditText pass=(EditText) findViewById(R.id.pass);
+
+        Button b1=(Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nam=name.getText().toString();
+                String pas=pass.getText().toString();
+
+                Toast.makeText(MainActivity.this, "Success-> Username: "+nam+" Password: "+pas, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
